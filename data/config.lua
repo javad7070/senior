d55c79695d7d50c3a339358b861b0e47cf5f6e14
Mr.javad <javadmohamadi70001377@gmail.com>
2017-06-03 19:59:18 +0430

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,22 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun"
+  },
+  info_text = "*》Smart Helper Bot V1.0*\n`》An helper bot of` *Smart* `based on` *BDMessenger*\n\n*》Admins :*\n*》Developer :* [XeonBoy](Telegram.Me/xeonboyy)\n_》Sponser And Manager:_ [∇َ↯ΨM̴ـــــ✪ــــMΨ̴َ↯َ∇ ََ™](Telegram.Me/Taslim_nmisham)\n\n*》Special thanks to :*\n》[NExeN](Telegram.Me/mahanshanbedi)\n\n*》Our channel :*\n》[SmartTeam](Telegram.Me/Smart_Ch_One)\n\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    322334261,
+	362194203
+  }
+}
+return _
+end
