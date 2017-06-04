ed7d8aee3b5796f87c6758657a51b8ba0c9b4425
Mr.javad <javadmohamadi70001377@gmail.com>
2017-06-04 18:32:12 +0430

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -9,6 +9,8 @@ do local _ = {
     "tools",
     "rmsg_all",
       "cldel",
+      "write",
+    "font",
     "fun"
   },
   info_text = "*》Smart Helper Bot V1.0*\n`》An helper bot of` *Smart* `based on` *BDMessenger*\n\n*》Admins :*\n*》Developer :* [XeonBoy](Telegram.Me/xeonboyy)\n_》Sponser And Manager:_ [∇َ↯ΨM̴ـــــ✪ــــMΨ̴َ↯َ∇ ََ™](Telegram.Me/Taslim_nmisham)\n\n*》Special thanks to :*\n》[NExeN](Telegram.Me/mahanshanbedi)\n\n*》Our channel :*\n》[SmartTeam](Telegram.Me/Smart_Ch_One)\n\n",
