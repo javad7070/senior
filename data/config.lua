4c8d97db63f204338546df0a8e80e1c56f22aebf
Mr.javad <javadmohamadi70001377@gmail.com>
2017-06-06 19:06:54 +0430

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -18,6 +18,8 @@ do local _ = {
     "fw_id",
     "maqam",
     "report",  
+    "me",
+    "rmsg",  
     "fun"
   },
   info_text = "*》Senior Bot V6.0*\n\n*》Admins :*\n*》Developer :* [javadmohammadi](Telegram.Me/cpp_cs)\n\n*》Our channel :*\n》[SeniorTeam](Telegram.Me/SeniorTm)\n\n",
